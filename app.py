9728b60fa53babc9e460ebaf56b7f7de292bcc99
kimhyejinnn <gpwls0735@gmail.com>
2024-07-24 11:49:53 +0900

    최종

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import random as rd
 
 st.header('주사위 게임', divider='rainbow')
 clicked = st.button('주사위던지기', type='primary')
+
+if clicked:
+    n = rd.randint(1, 6)
+    st.image(f'./img/{n}.png')
