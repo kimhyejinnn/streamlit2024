3d9a63b82189dd01fa2ec8023032aa0084384c2d
kimhyejinnn <gpwls0735@gmail.com>
2024-07-24 11:35:13 +0900

    수정

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.write('hello world')
 st.write('kimhyejin')
 st.markdown('hello **김혜진!**')
+
+st.header('This is a header with a divider', divider='rainbow')
+st.header('_Streamlit_ is :blue[cool] :sunglasses:')
